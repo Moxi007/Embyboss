b0435effbc2254e1717bdea1423b0e8ac1fc78dd
Moxi007 <lfy16800@gmail.com>
2026-03-07 01:00:40 +0800

    修复：移除__init__.py中已删除的captcha_cb导入（会导致启动崩溃）

--- bot/modules/callback/__init__.py
+++ bot/modules/callback/__init__.py
@@ -7,4 +7,3 @@ from .leave_delemby import leave_del_emby
 from .leave_unauth_group import anti_use_bot
 from .on_inline_query import find_sth_media
 from .close_it import close_it
-from .captcha_cb import on_captcha
